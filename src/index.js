8df1097dc9a084302fec11feef5bf60bb6c8eb38
sandayazisp <sandayazisp12@gmail.com>
2022-06-17 20:58:03 +0700

    membuat counter menggunakan state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import ContactApp from './components/ContactApp';
+import CounterApp from './components/CounterApp';
 
 // style
 import './styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<ContactApp />);
+root.render(<CounterApp />);
